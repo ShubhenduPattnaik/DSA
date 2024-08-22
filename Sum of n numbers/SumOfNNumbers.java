b5b9e63ae72dfe2ba85f7b65ca9c8be4d6001d86
Shubhendu Pattnaik <pattnaikshubhendu@gmail.com>
2024-08-23 01:02:56 +0530

    Updated the logic to remove explicit conversion

--- Sum of n numbers/SumOfNNumbers.java
+++ Sum of n numbers/SumOfNNumbers.java
@@ -4,6 +4,6 @@ class SumOfNNumber{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println((int)(n*(n+1)/2));
+        System.out.println(((n*(n+1))/2));
     }
 }
